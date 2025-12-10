ea0a49eeb5c9bcdf27292cba537d7baa3e00d792
palminjae <palminjae@gmail.com>
2025-12-10 15:32:01 +0900

    파일제거

--- universitylife/app/src/main/java/com/example/universitylife/meal/MealActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.universitylife.meal;
-
-public class MealActivity {
-}
--- universitylife/app/src/main/java/com/example/universitylife/model/ScheduleType.java
+++
@@ -1,6 +0,0 @@
-package com.example.universitylife.schedule.model;
-
-public enum ScheduleType {
-    CLASS,      // 수업
-    PERSONAL    // 개인 일정
-}
